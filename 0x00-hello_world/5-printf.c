919bc87623e4500ad3b47cd44ddbca663e8e4b7a
Chidubem <krain9421@gmail.com>
2022-09-09 01:09:41 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - main function, program starts here
+*
+* Description: Program prints out text using printf() function
+* Return: 0 if program runs successfully
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art\n");
+return (0);
+}
