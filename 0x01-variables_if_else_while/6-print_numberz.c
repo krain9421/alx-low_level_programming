2a83b80d9ec62c5aa01b69ba94fa13cd0bd66b79
Chidubem <krain9421@gmail.com>
2022-09-10 01:40:43 +0000

    Added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - main function, program starts here
+*
+* Description: Prints all single digit Base10 numbers (putchar)
+* Return: 0 if program runs successfully
+*/
+int main(void)
+{
+	int i;
+	char n = '1';
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar(n);
+	}
+	printf("\n");
+	return (0);
+}
