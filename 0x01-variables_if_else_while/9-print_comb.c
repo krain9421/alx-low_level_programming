285984ccfb6e37427eeddcc4abcc17fb885407fd
Chidubem <krain9421@gmail.com>
2022-09-10 03:30:00 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - main function, program starts here
+*
+* Description: Prints all combination of single-digit numbers
+* Return: 0 if program runs successfully
+*/
+int main(void)
+{
+	int i;
+	int num = 48;
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar(num++);
+	putchar(",");
+	putchar(" ");
+	}
+	return (0);
+}
