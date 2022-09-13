4204efd23ceb39c5560d7100e19edc73bf69cc83
Nwigwe Chidubem <krain9421@gmail.com>
2022-09-13 09:23:58 -0700

    Added main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,6 +9,10 @@
 
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+  int i = 0;
+  int wrt[8] = {95, 112, 117, 116, 99, 104, 97, 114};
+  for(; i < 8; i++)
+    _putchar(wrt[i]);
+  _putchar('\n');
+  return (0);
 }
