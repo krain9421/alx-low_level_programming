6f0a442f83d053a65d1db25745bbcdf6214d9e1d
Chidubem <krain9421@gmail.com>
2022-09-13 06:18:12 +0000

    Added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - main function, program starts here
+*
+* Description: Prints "_putchar"
+* Return: 0 if program runs successfully
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
