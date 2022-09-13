38d21dd8c6d5160afdbed327533a3c4166d34da9
Nwigwe Chidubem <krain9421@gmail.com>
2022-09-13 11:04:34 -0700

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet - Prints the alphabet in lowercase
+*
+* Return - void
+*/
+
+void print_alphabet()
+{
+int a = 97;
+
+for(; a < 123; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
