68794a7da3fd0addf1b2f646a0c1160334b59e6e
Nwigwe Chidubem <krain9421@gmail.com>
2022-09-13 11:25:40 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Prints the alphabets in lowercase 10 times
+* 
+* Return - void
+*/
+
+void print_alphabet_x10(void)
+{
+int i = 0;
+int j = 97;
+
+for (; i < 10; i++)
+{
+j = 97;
+for (; j < 123; j++)
+{
+_putchar(j);
+}
+_putchar('\n')
+}
+}
