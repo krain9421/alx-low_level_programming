b1bee30e1986a9887ece8ddc168e78b2496b2792
Nwigwe Chidubem <krain9421@gmail.com>
2022-09-13 19:21:51 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - Adds two integers and returns the result
+* @n: the number to be checked
+* Return: the sum of the two integers
+*/
+
+int add(int n, int m)
+{
+	return (n + m);
+}
+
