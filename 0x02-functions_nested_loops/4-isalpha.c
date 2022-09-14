e6f8fd13b72beabf1b254c28a75369d2b2507c68
Nwigwe Chidubem <krain9421@gmail.com>
2022-09-13 18:01:49 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,9 +7,9 @@
 * Return 0 if otherwise
 */
 
-int _isalpha(char c)
+int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90:))
 	{
 		return (1);
 	}
