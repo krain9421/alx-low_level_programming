dcea4d17b560c5f5b5e361ff2f936cf530e1931c
Cwigwe Chidubem William <krain9421@gmail.com>
2022-09-16 10:43:15 +0000

    101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_number - prints a number
+* print_number - prints a number using only _putchar
 * @n: the number to be printed
 *
 * Return: void
@@ -9,16 +9,34 @@
 
 void print_number(int n)
 {
+	int tmp = n;
+	int dig = 0;
+	int tdig;
+	int i;
+	int k;
+
+	do
+	{
+		dig++;
+		tmp = tmp / 10;
+	}while (tmp);
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
+		num = num * -1;
 	}
-	if (n / 10)
+
+	for (i = 0; i < dig; i++)
 	{
-		print_number(n / 10);
+		tdig = dig;
+		tmp = num;
+		k = i;
+		while (tdig > (k + 1))
+		{
+			tmp = tmp / 10;
+			--tdig;
+		}
+		_putchar('0' + (tmp % 10));
 	}
-
-	_putchar('0' + (n % 10));
 }
-
