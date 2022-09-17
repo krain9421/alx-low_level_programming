a8d9e52f398c7e95f7ea881efc48f1b3c6b4fdb9
Cwigwe Chidubem William <krain9421@gmail.com>
2022-09-17 20:32:20 +0000

    commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - program starts here
+*
+* Return: 0 if run successfully
+*/
+
+int main(void)
+{
+	int sum, t1, t2, next;
+
+	t1 = 0;
+	t2 = 1;
+	next = t1 + t2;
+
+	while (next < 4000000)
+	{
+		if (next % 2 == 0)
+		{
+			sum += nextTerm;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
+
