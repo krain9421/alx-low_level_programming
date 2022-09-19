f1c01d0628b351f5a8b4e9317f83bf4b522f0b42
Cwigwe Chidubem William <krain9421@gmail.com>
2022-09-19 16:50:00 +0000

    _puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _puts - function prints a string, followed by a new line
+* @str: string to be printed
+*
+* Return: void
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+}
+
