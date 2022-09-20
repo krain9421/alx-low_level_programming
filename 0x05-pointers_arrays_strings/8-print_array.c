8ddf1b475b5f28631f22e67de73f349cf007d02c
Cwigwe Chidubem William <krain9421@gmail.com>
2022-09-20 02:03:31 +0000

    Print Array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -18,13 +18,12 @@ void print_array(int *a, int n)
 		if (i == (n - 1))
 		{
 			printf("%d", a[i]);
+			 _putchar('\n');
 		}
 		else
 		{
 			printf("%d, ", a[i]);
 		}
 	}
-
-	_putchar('\n');
 }
 
