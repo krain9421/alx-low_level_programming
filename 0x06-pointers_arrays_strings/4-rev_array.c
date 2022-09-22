ab936fa08292a34589d1010d441c468c0252aea3
Nwigwe Chidubem <krain9421@gmail.com>
2022-09-22 14:55:34 +0000

    Added 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,15 +10,15 @@
 
 void reverse_array(int *a, int n)
 {
-	int i = n - 1;
+	int i = 0;
 	int tmp;
 
-	while (i > 0)
+	while (i < n / 2)
 	{
 		tmp = a[i];
-		a[i] = a[n - i - 1];
+		a[i] = a[n - i - 1];i
 		a[n - i - 1] = tmp;
-		i--;
+		i++;
 	}
 }
 
