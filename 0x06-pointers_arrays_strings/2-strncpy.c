c087a233ff03e055e84cece6fd0666c6a92d94ae
Nwigwe Chidubem <krain9421@gmail.com>
2022-09-22 13:43:44 +0000

    Added 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strncpy - function copies src to dest
+* @dest: string to copy into
+* @src: string to copy from
+* @n: max number of bytes to be copied from src
+*
+* Return: string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && (src[i] != '\0'))
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
