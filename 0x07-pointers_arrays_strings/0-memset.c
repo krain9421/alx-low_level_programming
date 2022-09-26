aadf59b936400c76194257101a3dbf5496fd8f2f
Nwigwe Chidubem <krain9421@gmail.com>
2022-09-26 14:54:10 +0000

    Added 0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,10 +11,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 	char **sptr = &s;
 
-	for (; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
