42423057dee1dad6f769438a3e5280e77e5990cf
Nwigwe Chidubem <krain9421@gmail.com>
2022-09-27 04:19:10 +0000

    commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+* _strspn - function that gets the length of a prefix substring
+* @s: string whose length to be checked
+* @accept: prefix substring
+*
+* Return: integer length of substring
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	char *find = NULL;
+	char *c;
+	unsigned int len = 0;
+
+	while (*s)
+	{
+		c = accept;
+		do {
+			if (*c == *s)
+			{
+				find = s;
+				break;
+			}
+		} while (*c++);
+
+		if (find != NULL)
+		{
+			len++;
+		}
+		else
+		{
+			s++;
+		}
+		
+	}
+
+	return (len);
+}
+
