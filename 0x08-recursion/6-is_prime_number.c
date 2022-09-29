afcb54226d1b1f776c343513cb40cf7095b4f597
Nwigwe Chidubem <krain9421@gmail.com>
2022-09-29 02:59:51 +0000

    is_prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* prime_checker - function checks for prime number
+* @n: number to check
+*
+* Return: 1 if prime, 0 otherwise
+*/
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n == 2 || n == 3 || n == 5 || n == 7)
+	{
+		return (0);
+	}
+	else if ((n % 2) && (n % 3) && (n % 5) && (n % 7))
+	{
+		return (1);
+	}
+	return (0);
+}
+
