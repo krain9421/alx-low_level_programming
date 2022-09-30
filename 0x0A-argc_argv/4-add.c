5e6acdfa96d073685f6bb63f1c536140fd3739ab
Nwigwe Chidubem <krain9421@gmail.com>
2022-09-30 13:29:32 +0000

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+* main - program that prints its name,
+*  followed by a new line
+* @argc: argument count
+* @argv: argument strings vector
+*
+* Return: 0 if run successfully,
+* 1 otherwise
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		int n;
+		int sum = 0;
+		
+		argc--;
+		while (argc)
+		{
+			if (isdigit(*argv[argc]))
+			{
+				n = atoi(argv[argc]);
+				sum += n;
+			}
+			else if (!isdigit(*argv[argc]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			argc--;
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+
+	printf("0\n");
+	return (0);
+}
+
