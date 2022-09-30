390b5bb7393c6131658ad7127be215ef12b37f02
Nwigwe Chidubem <krain9421@gmail.com>
2022-09-30 11:53:28 +0000

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - program that prints its name,
+*  followed by a new line
+* @argc: argument count
+* @argv: argument strings vector
+*
+* Return: 0 if run successfully
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+C - argc, argv
+0-whatsmyname - A program that prints its name, followed by a new line.
+
