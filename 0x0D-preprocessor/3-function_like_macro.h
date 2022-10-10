42989d2f05155fe45fb3fb5defe9c7199297d076
Nwigwe Chidubem <krain9421@gmail.com>
2022-10-10 19:57:06 +0000

    function_like_macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,9 +1,6 @@
-#ifndef MASK
-#define MASK
 #ifndef ABS
 #define ABS
 
-#define MASK(x) ((x) >> (4 * 8 - 1))
-#define ABS(x) ( (MASK(x)) + (x) ^ (MASK(x)) )
+#define ABS(x) (((x) < (0)) ? (-(x)) : (x)) 
 #endif
 
