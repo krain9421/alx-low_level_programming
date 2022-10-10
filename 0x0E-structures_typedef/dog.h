43d699757b6edbc529c9f38d68e7e719c26a89ca
Nwigwe Chidubem <krain9421@gmail.com>
2022-10-10 21:04:12 +0000

    init-dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
 /**
 * struct dog - dog struct
 * @name: name of dog
@@ -16,5 +14,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
 
