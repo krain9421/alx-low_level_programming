c77801892ec8e298c56b3918b041f326890356c8
Nwigwe Chidubem <krain9421@gmail.com>
2022-10-10 21:27:12 +0000

    print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,14 +14,17 @@ void print_dog(struct dog *d)
 	if (d == 0)
 		return;
 
-	else if
+	else
 	{
 		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
 
-		printf("Age: %f\n", d->age);
+		if (d->age == NULL)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d->age);
 
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
