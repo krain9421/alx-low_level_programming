18ff651a1d8f0f19fae00b2fb69d6412b3a554b8
Nwigwe Chidubem <krain9421@gmail.com>
2022-10-11 04:57:22 +0000

    free-dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* free_dog - a function that frees dog pointer
+* @d: struct dog pointer
+* Return: void
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d == 0)
+	{
+		return;
+	}
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
+
