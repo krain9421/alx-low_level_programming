9e8da79719981549d3f40ea145522e15037c85b1
Nwigwe Chidubem <krain9421@gmail.com>
2022-10-12 15:24:26 +0000

    print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
 * print_name - function that prints a name
@@ -10,9 +11,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
 	f(name);
 }
 
