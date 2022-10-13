6ef8c06b6f61b3dcbae147479b81c140ce1df8e2
Nwigwe Chidubem <krain9421@gmail.com>
2022-10-13 05:15:50 +0000

    op_functions_correct

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	{
 		n1 = atoi(argv[1]);
 		n2 = atoi(argv[3]);
-		i = (*get_op_func(argv[2]))((n1), (n2));
+		i = get_op_func(argv[2])((n1), (n2));
 		printf("%d\n", i);
 
 		return (0);
