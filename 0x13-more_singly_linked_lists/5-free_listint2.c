5eeb661ea28f5ce8bb80ad9b14378512310b707f
Nwigwe Chidubem <krain9421@gmail.com>
2022-10-26 19:55:46 +0000

    commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* free_listint2 - function that frees a listint_t list
+* @head: double pointer to the list to be freed
+*
+* Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+	listint_t *next;
+
+	while (temp != NULL)
+	{
+		next = temp->next;
+		free(temp);
+		temp = next;
+	}
+	*head = NULL;
+}
+
