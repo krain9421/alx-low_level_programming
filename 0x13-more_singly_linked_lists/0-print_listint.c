8e5f6124fb1015f001592f11239e0f66411ac837
Nwigwe Chidubem <krain9421@gmail.com>
2022-10-26 06:50:52 +0000

    print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -20,13 +20,10 @@ size_t print_listint(const listint_t *h)
 
 	for (; h != NULL; h = h->next)
 	{
-		if (h->n == NULL)
-		{}
-		else
-			printf("%d\n", h->n);
-
+		printf("%d\n", h->n);
 		count++;
 	}
+
 	return (count);
 }
 
