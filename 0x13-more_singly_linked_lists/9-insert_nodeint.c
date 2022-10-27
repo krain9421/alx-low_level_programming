ac84ee9f785a722f1875f69c8f3031ca6091bf0c
Nwigwe Chidubem <krain9421@gmail.com>
2022-10-27 02:16:41 +0000

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new;
 	unsigned int i = 0;
-	int count = 0;
+	unsigned int count = 0;
 	listint_t *temp = *head;
 
 	while (temp != NULL)
