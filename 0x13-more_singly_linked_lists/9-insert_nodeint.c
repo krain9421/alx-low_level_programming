5365e6f3609747dc46c08ee79dd8bf4a2ed28a7b
Nwigwe Chidubem <krain9421@gmail.com>
2022-10-27 02:30:46 +0000

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,14 +30,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	new->n = n;
-	new->next = NULL;
-
-	if (*head == NULL)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
 
 	else if (idx < count)
 	{
