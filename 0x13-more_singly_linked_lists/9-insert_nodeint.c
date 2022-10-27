fd4c70dbee215addc56d11dd5c54ef1b57b7dfc3
Nwigwe Chidubem <krain9421@gmail.com>
2022-10-27 03:02:06 +0000

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,36 +14,43 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new;
+	listint_t *temp = *head;
 	unsigned int i = 0;
 	unsigned int count = 0;
-	listint_t *temp = *head;
 
 	while (temp != NULL)
 	{
 		temp = temp->next;
 		count++;
 	}
-
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
 	new->n = n;
-
-	if (idx < count)
+	if (*head == NULL)
 	{
-		while (*head != NULL)
-		{
-			if (i == idx)
-			{
-				new->next = *head;
-				*head = new;
-				return (new);
-			}
-			i++;
-			head = &(*head)->next;
-		}
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	else if (idx > 0 && idx < count)
+	{
+		temp = *head;
+		for (i = 1; i < idx; i++)
+			temp = temp->next;
+
+		new->next = temp->next;
+		temp->next = new;
+		return (new);
 	}
 	return (NULL);
 }
