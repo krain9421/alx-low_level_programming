b777de06a8d86a79484b7a683d4b8d0b7858d676
Nwigwe Chidubem <krain9421@gmail.com>
2022-10-28 03:54:57 +0000

    get_endianness

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,7 @@ unsigned int binary_to_uint(const char *b);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
+int get_endianness(void);
 
 #endif
 
