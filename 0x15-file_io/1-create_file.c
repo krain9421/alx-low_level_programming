3e7ce3c82c1bf89159ae3f8e8b0dfc013ea8f44d
Nwigwe Chidubem <krain9421@gmail.com>
2022-11-01 01:40:34 +0000

    create-file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* create_file - creates a file
+* @filename: name of file
+* @text_content: text to be written into file
+* Return: 1 if successful 
+* -1 if failed
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fdes = 0, letters = 0, wrt = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fdes = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fdes < 0)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while(text_content[letters])
+		letters++;
+
+	wrt = write(fdes, text_content, letters);
+	if (wrt < 0)
+		return (-1);
+
+	close(fdes);
+
+	return (1);
+}
+
