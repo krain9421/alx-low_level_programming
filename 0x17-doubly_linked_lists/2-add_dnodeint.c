df358412c1c8f1871d9abab7cc1006aaa49e346e
Nwigwe Chidubem <krain9421@gmail.com>
2022-12-09 03:53:01 +0000

    Commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,9 +21,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new->n = n;
-	new->next = *head;
-	new->prev = NULL;
-	*head = new;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		*head->prev = NULL;
+		*head->next = NULL;
+	}
+	else
+	{
+		*head->prev = new;
+		new->next = *head;
+		new->prev = NULL;
+		*head = new;
+	}
 
 	return (new);
 }
