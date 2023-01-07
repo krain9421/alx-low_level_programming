7f101b6df6d8d3b3e47bfa6925935ea0593e508c
krain9421 <krain9421@gmail.com>
2023-01-07 04:53:50 +0000

    commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,1 +1,11 @@
 #include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ *
+ * @ht: a pointer to the hash table array.
+ * @key: the key.
+ * @value: the value associated with the key.
+ * Return: 1 on success, 0 on error.
+ */
+
