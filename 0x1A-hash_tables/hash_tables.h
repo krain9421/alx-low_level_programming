31a754e721f2bbc9f0426621ae7ced45caef21a1
krain9421 <krain9421@gmail.com>
2023-01-07 04:35:22 +0000

    Added 5-hash_table_print

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -2,6 +2,7 @@
 #define _HASH_TABLES_H_
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -36,5 +37,5 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 char *hash_table_get(const hash_table_t *ht, const char *key);
-
+void hash_table_print(const hash_table_t *ht);
 #endif
