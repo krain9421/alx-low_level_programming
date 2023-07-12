4b8a6aa67154c8598b109baab4a668d92cf0a200
Nwigwe Chidubem <krain9421@gmail.com>
2023-07-12 21:06:15 +0000

    0-linear.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
+#include <stdio.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 #endif
